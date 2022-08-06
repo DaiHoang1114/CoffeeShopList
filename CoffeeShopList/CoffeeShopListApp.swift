ff05753144133bb4ae7d63564e2c490d067c5b40
DaiHoang <hoangquocdai1411@gmail.com>
2022-08-06 11:25:39 +0700

    Initial Commit

---
+++ CoffeeShopList/CoffeeShopListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopListApp.swift
+//  CoffeeShopList
+//
+//  Created by Tri Nguyen on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
